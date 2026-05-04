b59e40f86659cf1242073946adcbffd0a8c57d90
micklanes <micklanessz@gmail.com>
2026-05-04 16:06:30 -0400

    CORREGIR_CSS_PERDIDO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import './globals.css';
 import { isAdmin } from '@/lib/auth'
 
 const inter = Inter({ subsets: ['latin'] })
